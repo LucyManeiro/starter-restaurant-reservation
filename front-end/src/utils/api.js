e6898c922f9cc3d0713b7cc49986c3c1c1a6efe4
Lucy Maneiro <81823350+LucyManeiro@users.noreply.github.com>
2021-10-21 19:59:51 -0500

    Update api.js

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -8,7 +8,7 @@
  import formatReservationTime from "./format-reservation-date";
  
  const API_BASE_URL =
-   process.env.REACT_APP_API_BASE_URL || "https://restaurant-reservation-backend-oy5tv3scj-lucymaneiro.vercel.app";
+   process.env.REACT_APP_API_BASE_URL || "https://starter-restaurant-reservation-backend-9avffdj64-lucymaneiro.vercel.app";
  
  /**
   * 
